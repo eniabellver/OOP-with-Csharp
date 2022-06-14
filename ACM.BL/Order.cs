1c876c9d17298809a82e9857fdfc2798f2ce453a
Eni Bellver <Eni.Bellver@oag.com>
2022-06-14 14:38:59 +0100

    implement interface

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Acme.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    public class Order
+    public class Order : EntityBase, ILoggable
     {
         public Order() : this(0) // <-- default constructor
         {
@@ -30,7 +31,7 @@ namespace ACM.BL
         /// Validates order data
         /// </summary>
         /// <returns></returns>
-        public bool Validate()
+        public override bool Validate()
         {
             var isValid = true;
 
@@ -38,6 +39,9 @@ namespace ACM.BL
 
             return isValid;
         }
+
+        public string Log() =>
+            $"{OrderID}: Date: {this.OrderDate.Value.Date} Status: {this.EntityState.ToString()}";
 
         public override string ToString() => $"{OrderDate.Value.Date} ({OrderID})";
     }
